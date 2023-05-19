fe971aee8290e520ad9a7259ec49ec513a082497
Deep Pansara <pansaradeep@gmail.com>
2023-05-19 17:16:26 +0530

    add banner images

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var slide_index = 1;
+displaySlides(slide_index);
+function nextSlide(n) {
+    displaySlides(slide_index += n);
+}
+function currentSlide(n) {
+    displaySlides(slide_index = n);
+}
+function displaySlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("showSlide");
+    if (n > slides.length) { slide_index = 1 }
+    if (n < 1) { slide_index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slide_index - 1].style.display = "block";
+}
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
